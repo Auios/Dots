a8953d99bf44b4f74dbc8668b122f087c234594c
Auios <LoneAuios@gmail.com>
2019-03-10 22:37:08 -0400

    QuadTree_CPP

--- QuadTree/QuadTree/QuadTree.h
+++ QuadTree/QuadTree/QuadTree.h
@@ -15,19 +15,19 @@ struct QuadTree
 
 	int depth;
 	int maxDepth;
-	int count;
-	QuadTreeNode* p;
+	int count = 0;
+	QuadTreeNode* n = nullptr;
 
-	QuadTree* NW;
-	QuadTree* NE;
-	QuadTree* SW;
-	QuadTree* SE;
+	QuadTree* NW = nullptr;
+	QuadTree* NE = nullptr;
+	QuadTree* SW = nullptr;
+	QuadTree* SE = nullptr;
 };
 
 extern "C"
 {
 	QuadTree EXPORT qt_create(Rect area, int depth, int maxDepth);
-	void EXPORT qt_delete(QuadTree qt);
-	void EXPORT qt_add(QuadTree qt, void* n);
-	void* EXPORT qt_searchNearest(QuadTree qt, Point p);
+	void EXPORT qt_delete(QuadTree* qt);
+	void EXPORT qt_add(QuadTree* qt, void* n);
+	void* EXPORT qt_searchNearest(QuadTree* qt, Point p);
 }
